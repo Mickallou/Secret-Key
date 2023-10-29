489fb91be64802347adaa165122cdf55d083f601
Mickael Allouche <allouchemicka@gmail.com>
2023-10-29 20:58:55 +0200

    change keydown to keyup

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-document.addEventListener('keydown', function (secret) {
+document.addEventListener('keyup', function (secret) {
     if ((secret.key === "b" || secret.key === "B") && secret.ctrlKey) {
         alert('You find the Secret Key binding');
         document.getElementById('perfection').style.display = "block";
